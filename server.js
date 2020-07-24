dc4b93e9d8b2fb594911ad648af1c3cabfa38410
MikeMor-fr <62838338+MikeMor-fr@users.noreply.github.com>
2020-07-24 15:52:43 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const path = require('path')
+
+const items = require("./routes/api/items");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// DB config
+const db = require("./config/keys").mongoURI;
+
+// Conect to MongoDB
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log("mongoDB connected"))
+.catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/items', items);
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set a static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
+
+// Starting server
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
